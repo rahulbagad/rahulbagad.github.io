91b4b03d1ffacac3eb8aa4d01c987396690dbb33
rlbagad <rahul.bagad@walchandsangli.ac.in>
2017-07-01 23:45:56 +0530

    my profiles

--- js/main.js
+++ js/main.js
@@ -14,7 +14,6 @@
            });
          });
          
-
       document.addEventListener("DOMContentLoaded", func=function(){
          Typed.new("#typed", {
             strings: ["Programming","Machine Learning","Web Development",""],
@@ -26,13 +25,10 @@
          });
       });
 
-
 	$("a[href='#top']").click(function() {
 	  $("html, body").animate({ scrollTop: 0 }, "slow");
 	  return false;
 	});
-
-
 
 	$(document).scroll(function() {
 	  var y = $(this).scrollTop();
@@ -43,60 +39,42 @@
 	  }
 	});
 
-
-
          $(document).ready(function(){
               $("#puzz").click(function(){
                   $("#p").fadeToggle(1000);
               });
          });
          
-
-
           $(document).ready(function(){
               $("#gas").click(function(){
                   $("#g").fadeToggle(1000);
               });
           });
-
-
           $(document).ready(function(){
               $("#edu").click(function(){
                   $("#e").fadeToggle(1000);
               });
           });
-
-
           $(document).ready(function(){
               $("#quiz").click(function(){
                   $("#q").fadeToggle(1000);
               });
           });
-
-
           $(document).ready(function(){
                $(".landing").css("min-height", $( window ).height()- $(".nav-wrapper").height());
                $(".container").css("margin-top", $( window ).height()/25);
                $(".links").css("top", $( window ).height()*75/100)
                $(".links").css("left", $( window ).width()*75/100)
           });
-
-
           $(document).ready(function(){
          		$('.modal').modal();
          });
-
-
          $(document).ready(function(){
          $('#construction').modal('open');
          });
-
-
          $(document).ready(function(){
          $('.carousel').carousel();
          });
-
-
          $(document).ready(function(){
          $('.materialboxed').materialbox();
          });
