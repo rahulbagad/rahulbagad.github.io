2ddbd91de80e3644903d6d731657ce51f42fa081
rlbagad <rahul.bagad@walchandsangli.ac.in>
2017-07-01 22:24:26 +0530

    typed js

--- js/main.js
+++ js/main.js
@@ -14,10 +14,25 @@
            });
          });
          
+
+      document.addEventListener("DOMContentLoaded", func=function(){
+         Typed.new("#typed", {
+            strings: ["Programming","Machine Learning","Web Development",""],
+            typeSpeed: 50,
+            backDelay: 2000,
+            callback: function (){
+            func();
+          }
+         });
+      });
+
+
 	$("a[href='#top']").click(function() {
 	  $("html, body").animate({ scrollTop: 0 }, "slow");
 	  return false;
 	});
+
+
 
 	$(document).scroll(function() {
 	  var y = $(this).scrollTop();
@@ -28,42 +43,60 @@
 	  }
 	});
 
+
+
          $(document).ready(function(){
               $("#puzz").click(function(){
                   $("#p").fadeToggle(1000);
               });
          });
          
+
+
           $(document).ready(function(){
               $("#gas").click(function(){
                   $("#g").fadeToggle(1000);
               });
           });
+
+
           $(document).ready(function(){
               $("#edu").click(function(){
                   $("#e").fadeToggle(1000);
               });
           });
+
+
           $(document).ready(function(){
               $("#quiz").click(function(){
                   $("#q").fadeToggle(1000);
               });
           });
+
+
           $(document).ready(function(){
                $(".landing").css("min-height", $( window ).height()- $(".nav-wrapper").height());
                $(".container").css("margin-top", $( window ).height()/25);
                $(".links").css("top", $( window ).height()*75/100)
                $(".links").css("left", $( window ).width()*75/100)
           });
+
+
           $(document).ready(function(){
          		$('.modal').modal();
          });
+
+
          $(document).ready(function(){
          $('#construction').modal('open');
          });
+
+
          $(document).ready(function(){
          $('.carousel').carousel();
          });
+
+
          $(document).ready(function(){
          $('.materialboxed').materialbox();
          });
