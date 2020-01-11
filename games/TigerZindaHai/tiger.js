52424932be132aafaad2f75cb8d74e1be4566e14
rahulb <rahulb@sahajsoft.com>
2020-01-11 19:32:04 +0530

    collisionbug fix

--- games/TigerZindaHai/tiger.js
+++ games/TigerZindaHai/tiger.js
@@ -14,8 +14,8 @@ class Tiger {
   }
 
   hits(obstacle) {
-    let x1 = obstacle.x + obstacle.r * 0.5;
-    let y1 = obstacle.y + obstacle.r * 0.5;
+    let x1 = this.x + this.r * 0.5;
+    let y1 = this.y + this.r * 0.5;
     let x2 = obstacle.x + obstacle.r * 0.5;
     let y2 = obstacle.y + obstacle.r * 0.5;
     return collideCircleCircle(x1, y1, this.r, x2, y2, obstacle.r);
