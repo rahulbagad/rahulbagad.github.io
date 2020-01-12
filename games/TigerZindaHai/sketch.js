ed1c1bd0a212756bc70cca5d1dae886f7023b416
rahulb <rahulb@sahajsoft.com>
2020-01-12 13:34:12 +0530

    Threshold decrease for obstacles

--- games/TigerZindaHai/sketch.js
+++ games/TigerZindaHai/sketch.js
@@ -56,7 +56,7 @@ function keyPressed() {
 function draw() {
 
   if(startGame){
-    if (random(1) < 0.05) {
+    if (random(1) < 0.003) {
       updateScore();
       currentObstacle = obstacleList[obstacleCounter % obstacleList.length]
       obstacleCounter = obstacleCounter + 1;
